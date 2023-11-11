143592601d87cd3cbe9c0d9bbf2bc54715340f28
Bhurendra1 <149679502+Bhurendra1@users.noreply.github.com>
2023-11-11 03:56:26 +0000

    commit_config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 15422
+let accountEmail ="hitesh@google.com"
+var accountPassword = "145215"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "dkhekkf.com"
+accountPassword = "441514552"
+accountCity = "Begloare"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/* Prefer not to use var 
+beacause of issue in block scope and functional scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert ( 3+ 3) we using node js, not browser
+
+console.log(3 + 4);
+
+console.log("bhurendra")
+
+
+// let name = "bhurendra"
+// let age = 20
+// let is LoggedId = false
+
+// numer => 2 to power 53
+// bigint 
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
